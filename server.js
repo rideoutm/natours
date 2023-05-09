2d0248f4782a4bc57a949199f97f6de9e86954fe
Michael Rideout <rideout.m@hotmail.com>
2023-05-09 11:00:49 -0400

    build mongoose schema

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("CONNECTIONS: ", con.connections);
+    console.log("connection successful");
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [, "Must include a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "Must include a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+const testTour = new Tour({
+  name: "The park camper",
+  rating: 4.7,
+  price: 500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("ERROR: ", err);
+  });
 
 const app = require("./app");
 
